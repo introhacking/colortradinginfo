da670479082660e775acf82b9841611cd0e5985c
Manish Kumar Gupta <introhacking@gmail.com>
2025-07-16 14:06:39 +0530

    fix vite.config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,42 +1,41 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { BACKEND_URI } from './src/services/apiService'
 
 // Load env file based on `mode` (e.g. .env.development, .env.production)
 // const env = loadEnv(mode, process.cwd());
 
-// console.log(env)
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: `${BACKEND_URI}`,
-//         changeOrigin: true,
-//         secure: true
-//       },
-//     }
-//   },
-//   plugins: [react()],
-// })
-
-
-
-
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` (e.g. .env.development, .env.production)
-  const env = loadEnv(mode, process.cwd());
-  return {
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_BACKEND_URI,
-          changeOrigin: true,
-          secure: true,
-        },
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: BACKEND_URI,
+        changeOrigin: true,
+        secure: true
       },
-    },
-    plugins: [react()],
-  };
-});
+    }
+  },
+  plugins: [react()],
+})
+
+
+
+
+// export default defineConfig(({ mode }) => {
+//   // Load env file based on `mode` (e.g. .env.development, .env.production)
+//   const env = loadEnv(mode, process.cwd());
+//   return {
+//     server: {
+//       proxy: {
+//         '/api': {
+//           target: env.VITE_BACKEND_URI,
+//           changeOrigin: true,
+//           secure: true,
+//         },
+//       },
+//     },
+//     plugins: [react()],
+//   };
+// });
